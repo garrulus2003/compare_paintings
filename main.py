731475d6a0dcc59b8ca91e4edfa90037fdc246a7
garrulus2003 <54739772+garrulus2003@users.noreply.github.com>
2021-05-06 02:23:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,103 +4,80 @@ import re
 import urllib.request
 from PIL import Image, ImageTk
 from compare_images import get_image_hash, compare_hash
+from image_traits import post_photo, get_paintings, download_photo, find_similar
 
 
-f = open("artists_links.json", 'r')
-root = Tk()
-root.title("Поиск похожих картин.")
-root.geometry("800x500")
-variable = StringVar(root)
-variable.set("       Выберите художника")
-variable1 = StringVar(root)
-variable1.set("       Выберите художника")
-A = json.load(f)
-
-
-def get_paintings(painter):
-    url = A[painter]
-    content = urllib.request.urlopen(url).read()
-    imgUrls = re.findall('img .*?src="(.*?)"', str(content))
-    answer = []
-    for img in imgUrls:
-        if img.endswith(".jpg"):
-            try:
-                resource = urllib.request.urlopen(img)
-                answer.append(resource)
-                print(1)
-            except Exception as e:
-                print(e)
-        if len(answer) >= 15:
-            break
-    return answer
-
-
-def find_similar():
-    list1 = get_paintings(variable.get())
-    list2 = get_paintings(variable1.get())
-
-    for i in range(len(list1)):
+def download_list(source_list, target_file):
+    """
+    downloads all images from the list, does not break in case of exception
+    :param source_list: list of jpg files
+    :param target_file: a string able to format
+    :return:
+    """
+    for i in range(len(source_list)):
         try:
-            f1 = open("img1_{}.jpg".format(i), 'wb')
-            f1.write(list1[i].read())
-            f1.close()
+            download_photo(source_list[i].read(), target_file.format(i))
         except:
-            print(1)
+            pass
 
-    for i in range(len(list2)):
-        try:
-            f2 = open("img2_{}.jpg".format(i), 'wb')
-            f2.write(list2[i].read())
-            f2.close()
-        except:
-            print(1)
 
-    B = []
+def find_similar(list1, list2, screen):
+    """
+    posts two most similar photos
+    :param screen: tkinter root
+    :param list1: list of links to images
+    :param list2: list of links to images
+    :return:
+    """
+    download_list(list1, "img1_{}.jpg")
+    download_list(list2, "img2_{}.jpg")
+
+    compare_matrix = [[0]*len(list2)]*len(list1)
     i_min = 0
     j_min = 0
     min_value = 100
     for i in range(len(list1)):
-        B.append([])
         for j in range(len(list2)):
-            B[i].append(compare_hash(get_image_hash("img1_{}.jpg".format(i)), get_image_hash("img2_{}.jpg".format(j))))
-            if B[i][j] < min_value:
+            compare_matrix[i][j] = compare_hash(get_image_hash("img1_{}.jpg".format(i)),
+                                                get_image_hash("img2_{}.jpg".format(j)))
+            if compare_matrix[i][j] < min_value:
                 i_min = i
                 j_min = j
-                min_value = B[i][j]
+                min_value = compare_matrix[i][j]
 
-    print(i_min, j_min)
-    load = Image.open("img1_{}.jpg".format(i_min))
-    render = ImageTk.PhotoImage(load)
-    img = Label(root, image=render)
-    img.image = render
-    img.place(x=30, y=200)
-
-    load1 = Image.open("img2_{}.jpg".format(j_min))
-    render1 = ImageTk.PhotoImage(load1)
-    img1 = Label(root, image=render1)
-    img1.image = render1
-    img1.place(x=430, y=200)
+    post_photo("img1_{}.jpg".format(i_min), 30, 200, screen)
+    post_photo("img2_{}.jpg".format(j_min), 430, 200, screen)
 
 
-lbl = Label(text="Многие художники одного времени придерживаются одного стиля.")
-lbl1 = Label(text="В этом можно убедиться на примере художников первой половины XX века.")
-lbl2 = Label(text="Несмотря на то, что искусство этого времени отличается своим разнообразием,")
-lbl3 = Label(text="Вы увидете некоторое сходство.")
-btn = Button(text="Найти похожие картины", command=find_similar)
-w = OptionMenu(root, variable, *list(A))
-u = OptionMenu(root, variable1, *list(A))
+if __name__ == '__main__':
+    artists_links = open("artists_links.json", 'r')
+    root = Tk()
+    root.title("Поиск похожих картин.")
+    root.geometry("800x500")
+
+    artist1 = StringVar(root)
+    artist1.set("       Выберите художника")
+    artist2 = StringVar(root)
+    artist2.set("       Выберите художника")
+    artists = json.load(artists_links)
+
+    def find_similar_clicked():
+        find_similar(get_paintings(artists[artist1.get()]), get_paintings(artists[artist2.get()]), root)
 
 
-w.config(width=50, height=0)
-u.config(width=50, height=0)
+    lbl = Label(text="Многие художники одного времени придерживаются одного стиля.")
+    lbl1 = Label(text="В этом можно убедиться на примере художников первой половины XX века.")
+    lbl2 = Label(text="Несмотря на то, что искусство этого времени отличается своим разнообразием,")
+    lbl3 = Label(text="Вы увидете некоторое сходство.")
+    btn = Button(text="Найти похожие картины", command=find_similar_clicked)
 
-w.pack()
-u.pack()
+    choose1 = OptionMenu(root, artist1, *list(artists))
+    choose2 = OptionMenu(root, artist2, *list(artists))
+    choose1.config(width=50, height=0)
+    choose2.config(width=50, height=0)
 
-btn.pack()
-lbl.pack()
-lbl1.pack()
-lbl2.pack()
-lbl3.pack()
+    graphic_elements = [choose1, choose2, btn, lbl, lbl1, lbl2, lbl3]
+    for element in graphic_elements:
+        element.pack()
 
-root.mainloop()
+    root.mainloop()
